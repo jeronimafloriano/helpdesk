a3570b0c6dd1b520c9c190cf885e572ed3fe4539
jeronima <jeronima.floriano@totvs.com.br>
2023-02-21 12:26:38 -0300

    Adicionando validações de CPF e EMAIL já existentes no sistema, ao criar um novo ténico

--- src/main/java/com/jeronima/helpdesk/exceptions/handler/ResourceExceptionHandler.java
+++ src/main/java/com/jeronima/helpdesk/exceptions/handler/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jeronima.helpdesk.exceptions.handler;
 
+import com.jeronima.helpdesk.exceptions.DataIntegrityViolationException;
 import com.jeronima.helpdesk.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +18,13 @@ public class ResourceExceptionHandler {
         StandardError error = new StandardError(System.currentTimeMillis(),
                 HttpStatus.NOT_FOUND.value(), "Not Found", ex.getMessage(), request.getRequestURI());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException ex,
+                                                                 HttpServletRequest request){
+        StandardError error = new StandardError(System.currentTimeMillis(),
+                HttpStatus.BAD_REQUEST.value(), "Violação de dados", ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
     }
 }
--- src/main/java/com/jeronima/helpdesk/service/TecnicoService.java
+++ src/main/java/com/jeronima/helpdesk/service/TecnicoService.java
@@ -2,6 +2,8 @@ package com.jeronima.helpdesk.service;
 
 import com.jeronima.helpdesk.domain.Tecnico;
 import com.jeronima.helpdesk.dto.TecnicoDto;
+import com.jeronima.helpdesk.exceptions.DataIntegrityViolationException;
+import com.jeronima.helpdesk.repository.PessoaRepository;
 import com.jeronima.helpdesk.repository.TecnicoRepository;
 import com.jeronima.helpdesk.exceptions.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
@@ -12,9 +14,11 @@ import java.util.stream.Collectors;
 @Service
 public class TecnicoService {
     private final TecnicoRepository repository;
+    private final PessoaRepository pessoaRepository;
 
-    public TecnicoService(TecnicoRepository repository) {
+    public TecnicoService(TecnicoRepository repository, PessoaRepository pessoaRepository) {
         this.repository = repository;
+        this.pessoaRepository = pessoaRepository;
     }
 
     public Tecnico findById(Integer id){
@@ -28,7 +32,24 @@ public class TecnicoService {
     }
 
     public Tecnico create(TecnicoDto dto) {
+        validaCpf(dto.getCpf());
+        validaEmail(dto.getEmail());
+
         var tecnico =  new Tecnico(dto.getNome(), dto.getCpf(), dto.getEmail(), dto.getSenha());
         return repository.save(tecnico);
     }
+
+    private void validaCpf(String cpf) {
+       pessoaRepository.findByCpf(cpf).ifPresent(x -> {
+           throw new DataIntegrityViolationException("CPF já cadastrado no sistema");
+       });
+
+    }
+
+    private void validaEmail(String email) {
+        pessoaRepository.findByEmail(email).ifPresent(x -> {
+            throw new DataIntegrityViolationException("Email já cadastrado no sistema");
+        });
+    }
+
 }
